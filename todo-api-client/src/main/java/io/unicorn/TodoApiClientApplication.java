10499aae907fa090b16f5df46ee54ab0b3f3c22d
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2017-04-04 14:56:01 +0200

    added missing dependency mime-type for /hystrix.stream

--- todo-api-client/src/main/java/io/unicorn/TodoApiClientApplication.java
+++ todo-api-client/src/main/java/io/unicorn/TodoApiClientApplication.java
@@ -1,10 +1,14 @@
 package io.unicorn;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +26,7 @@ public class TodoApiClientApplication {
         SpringApplication.run(TodoApiClientApplication.class, args);
     }
 
-    @FeignClient(url = "http://localhost:8080", name = "todo-api")
+    @FeignClient(url = "http://localhost:8080", name = "todo-api", fallback = TodoClientFallback.class)
     public interface TodoClient {
 
         @RequestMapping(value = "/api/todos", method = RequestMethod.GET)
@@ -49,5 +53,10 @@ public class TodoApiClientApplication {
         public ResponseEntity<List<TodoDto>> all() {
             return todoClient.getAll();
         }
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), "/hystrix.stream");
     }
 }
--- todo-api/src/main/java/io/unicorn/TodoApiApplication.java
+++ todo-api/src/main/java/io/unicorn/TodoApiApplication.java
@@ -2,8 +2,10 @@ package io.unicorn;
 
 import com.google.common.collect.Lists;
 import lombok.Data;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,16 +15,29 @@ import java.util.UUID;
 @SpringBootApplication
 public class TodoApiApplication {
 
+    private static final List<Todo> db = Lists.newArrayList();
     private static int count = 0;
+
     public static void main(String[] args) {
         SpringApplication.run(TodoApiApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            for (int i = 0; i < 100; i++) {
+                Todo t = new Todo();
+                t.setId(count++);
+                t.setTitle("Hello there " + System.currentTimeMillis());
+                db.add(t);
+            }
+        };
+    }
+
+
     @RestController
     @RequestMapping("/api/todos")
     public class TodoController {
-
-        private final List<Todo> db = Lists.newArrayList();
 
         @GetMapping
         public ResponseEntity<List<Todo>> findAll() {
@@ -43,9 +58,5 @@ public class TodoApiApplication {
     public class Todo {
         private long id;
         private String title;
-
-        public int generateId() {
-            return UUID.randomUUID().clockSequence();
-        }
     }
 }
